09cecb07260d6f0ba5c071613c3a092c823665ee
ajshemi <akpojotor@yahoo.com>
2020-03-06 17:58:05 -0500

    updated chartscontainer and charts css

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -13,7 +13,8 @@ class ChartsContainer extends Component {
     //    let {sadness,joy,fear,anger,disgust}=this.props?.emotions[0]
        let {emotions:arrayEmotions}=this.props
        let arrayOfEmotionsData=this.props.emotions.slice(arrayEmotions.length-5,arrayEmotions.length).map(emotion=>[emotion.sadness,emotion.joy,emotion.fear,emotion.anger,emotion.disgust])
-       let arrayOfUserId=this.props.emotions.map(emotion=>emotion.user_id)
+       let arrayOfUserId=this.props.emotions.slice(arrayEmotions.length-5,arrayEmotions.length).map(emotion=>emotion.user_id)
+    //    arrayOfEmotionsData.length-5,arrayOfEmotionsData.length
        let chartdata=arrayOfEmotionsData.map((array,index)=>{
            return {label:`User ${arrayOfUserId[index]}`,data:array,backgroundColor:`rgba(${255-(Math.floor(Math.random() * 50) + 1)}, ${159-(Math.floor(Math.random() * 100) + 1)}, ${64+(Math.floor(Math.random()*5)+1)})`}}
         )
@@ -24,7 +25,9 @@ class ChartsContainer extends Component {
         // console.log(chartData1)
         return (
              <div>
+                 <p className='charttext'>IBM Watson NLU Emotions Analysis</p>
                  <Chart chartData={chartData} legendPosition="top"/>
+                 <p className='charttext'>5 Most Recent Comments </p>
              </div> );
     }
 }
